0c6279218513081ceb67e011d9b166c78b28033c
bu2terFLY <119470972+bu2terFLY@users.noreply.github.com>
2023-03-26 16:42:30 +0530

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -3,6 +3,6 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 @Client.on_message(filters.private & filters.command(["refer"]))
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
-       		[ [ InlineKeyboardButton("Share Your Link" ,url=f"https://t.me/share/url?url=https://t.me/rename_urbot?start={message.from_user.id}") ]   ])
-    await message.reply_text(f"Refer And Earn Get 100MB Upload Limit\nPer Refer 100MB\n Your Link :- https://t.me/rename_urbot?start={message.from_user.id} ",reply_to_message_id = message.id,reply_markup=reply_markup,)
+       		[ [ InlineKeyboardButton("SHARE REFERAL🚀" ,url=f"https://t.me/share/url?url=https://t.me/Thumbnail_editor_4gb_bot?start={message.from_user.id}") ]   ])
+    await message.reply_text(f"🤯sʜᴀʀᴇ ᴛʜɪs ʙᴏᴛ ᴡɪᴛʜ ғʀɪᴇɴᴅs🍳 ᴀɴᴅ ɢᴇᴛ ᴜᴘᴛᴏ 5ɢʙ💥 ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ / ᴘᴇʀ ʀᴇғᴇʀᴀʟ \n\n👇🏻ʜᴇʀᴇ ʏᴏᴜʀ 🎟️ʀᴇғᴇʀᴀʟ ᴄᴏᴅᴇ👇🏻",reply_to_message_id = message.id,reply_markup=reply_markup,)
     
