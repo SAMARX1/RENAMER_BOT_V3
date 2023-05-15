84bd9ea456c27a23c1432633022a7dafac10d692
SAMARX1 <123295466+SAMARX1@users.noreply.github.com>
2023-05-15 14:01:53 +0530

    Update refer.py

--- plugins/refer.py
+++ plugins/refer.py
@@ -4,5 +4,5 @@ from pyrogram.types import ( InlineKeyboardButton, InlineKeyboardMarkup,ForceRep
 async def refer(client,message):
     reply_markup = InlineKeyboardMarkup(
        		[ [ InlineKeyboardButton("SHARE REFERAL🚀" ,url=f"https://t.me/share/url?url=https://t.me/Thumbnail_editor_4gb_bot?start={message.from_user.id}") ]   ])
-    await message.reply_text(f"🤯sʜᴀʀᴇ ᴛʜɪs ʙᴏᴛ ᴡɪᴛʜ ғʀɪᴇɴᴅs🍳 ᴀɴᴅ ɢᴇᴛ ᴜᴘᴛᴏ 5ɢʙ💥 ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ / ᴘᴇʀ ʀᴇғᴇʀᴀʟ \n\n👇🏻ʜᴇʀᴇ ʏᴏᴜʀ 🎟️ʀᴇғᴇʀᴀʟ ᴄᴏᴅᴇ👇🏻",reply_to_message_id = message.id,reply_markup=reply_markup,)
+    await message.reply_text(f"🤯sʜᴀʀᴇ  ᴛʜɪs  ʙᴏᴛ  ᴡɪᴛʜ  ғʀɪᴇɴᴅs🍳 ᴀɴᴅ  ɢᴇᴛ  ᴜᴘᴛᴏ  5ɢʙ💥 ᴜᴘʟᴏᴀᴅ  ʟɪᴍɪᴛ / ᴘᴇʀ  ʀᴇғᴇʀᴀʟ \n\n👇🏻ʜᴇʀᴇ  ʏᴏᴜʀ 🎟️ʀᴇғᴇʀᴀʟ  ᴄᴏᴅᴇ👇🏻",reply_to_message_id = message.id,reply_markup=reply_markup,)
     
