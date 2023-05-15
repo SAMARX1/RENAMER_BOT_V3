7d633c62a20d80a0670ef4aa2afb5d1a14d8b795
bu2terFLY <119470972+bu2terFLY@users.noreply.github.com>
2023-05-15 08:39:56 +0530

    Update and rename features.py to about.py

---
+++ plugins/about.py
@@ -0,0 +1,13 @@
+import os 
+from pyrogram import Client, filters
+token = os.environ.get('TOKEN','')
+botid = token.split(':')[0]
+from helper.database import botdata, find_one, total_user
+from helper.progress import humanbytes
+@Client.on_message(filters.private & filters.command(["about"]))
+async def start(client,message):
+	botdata(int(botid))
+	data = find_one(int(botid))
+	total_rename = data["total_rename"]
+	total_size = data["total_size"]
+	await message.reply_text(f"**⚡️ᴛᴏᴛᴀʟ ᴜsᴇʀs:- {total_user()}1\n⚡️ᴛᴏᴛᴀʟ ʀᴇɴᴀᴍᴇᴅ ғɪʟᴇs:- {total_rename}5\n⚡sɪᴢᴇ ᴏғ ʀᴇɴᴀᴍᴇᴅ ғɪʟᴇs:- {humanbytes(int(total_size))}\nᴠᴇʀsɪᴏɴ:6.3 \nᴜᴘᴅᴀᴛᴇᴅ ᴏɴ 2023/05/15 \n\n💡ᴛʀʏ ᴍᴏʀᴇ ғᴇᴀᴛᴜʀᴇs👇🏻 \n\n🚀𝘕𝘖𝘙𝘔𝘈𝘓 𝘍𝘌𝘈𝘛𝘜𝘙𝘌𝘚👇🏻 \n📌ʀᴇɴᴀᴍᴇ ғɪʟᴇ ᴏʀ ᴠɪᴅᴇᴏ \n📌sᴇᴛ ᴀᴜᴛᴏ ᴄᴀᴘᴛɪᴏɴ \n📌sᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ \n📌ғɪʟᴇ/ᴠɪᴅᴇᴏ ᴄᴏɴᴠᴇʀᴛᴇʀ \n\n🚀𝘗𝘙𝘌𝘔𝘐𝘜𝘔 𝘍𝘌𝘈𝘛𝘜𝘙𝘌𝘚👇🏻  \n📌4ɢʙ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ \n📌ᴠɪᴅᴇᴏ ᴛʀɪᴍ ᴀɴᴅ ᴍᴇʀɢᴇ \n📌ᴀᴜᴅɪᴏ ᴄʜᴀɴɢᴇ ᴏʀ ʀᴇᴍᴏᴠᴇ \n📌3x ᴅᴏᴡɴʟᴏᴀᴅ sᴘᴇᴇᴅ \n\n📦/upgrade ᴘʟᴀɴ ᴛᴏ ᴜsᴇ ᴘʀᴇᴍɪᴜᴍ ғᴇᴀᴛᴜʀᴇs**",quote=True)
