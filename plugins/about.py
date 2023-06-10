1138782449b5408154625b9f936607c389a6f836
bu2terFLY <119470972+bu2terFLY@users.noreply.github.com>
2023-06-10 16:48:23 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -10,8 +10,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"═════════════════════\n⚡ᴠᴇʀsɪᴏɴ:6.4 \n⚡ᴜᴘᴅᴀᴛᴇᴅ ᴏɴ 2023/06/10 \n
-
-▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n**ғᴇᴀᴛᴜʀᴇs👇🏻** \n\n📦ʀᴇɴᴀᴍᴇ  ғɪʟᴇs / ᴠɪᴅᴇᴏ  🆓\n📦ᴄᴏɴᴠᴇʀᴛ ғɪʟᴇ  ᴛᴏ ᴠɪᴅᴇᴏ  🆓\n📦sᴇᴛ  ᴛʜᴜᴍʙɴᴀɪʟ ᴏɴ ᴠɪᴅᴇᴏ 🆓 \n📦sᴇᴛ  ᴀᴜᴛᴏ ᴄᴀᴘᴛɪᴏɴ  🆓\n📦4 ɢʙ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 👑\n📦3x ᴅᴏᴡɴʟᴏᴀᴅ sᴘᴇᴇᴅ👑\n📦ᴛʀɪᴍ & ᴍᴇʀɢᴇ ᴠɪᴅᴇᴏ 💎\n📦ᴀᴜᴅɪᴏ ᴄʜᴀɴɢᴇ & ʀᴇᴍᴏᴠᴇᴅ 💎\n\n **🆓 = all users can use\n👑 = only premium users can use\n💎 = only DIAMOND users can use**\n\n⚡**/upgrade 𝗣𝗟𝗔𝗡 𝗧𝗢 𝗨𝗦𝗘 𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦** \n\n
-
-▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀",quote=True)
+	await message.reply_text(f"⚡ᴠᴇʀsɪᴏɴ:6.4 \n⚡ᴜᴘᴅᴀᴛᴇᴅ ᴏɴ 2023/06/10 \n\n**ғᴇᴀᴛᴜʀᴇs👇🏻** \n\n📦ʀᴇɴᴀᴍᴇ  ғɪʟᴇs ᴏʀ ᴠɪᴅᴇᴏ 🆓\n📦ᴄᴏɴᴠᴇʀᴛ ғɪʟᴇ ᴛᴏ ᴠɪᴅᴇᴏ 🆓\n📦sᴇᴛ ᴛʜᴜᴍʙɴᴀɪʟ ᴏɴ ᴠɪᴅᴇᴏ 🆓 \n📦sᴇᴛ ᴀᴜᴛᴏ ᴄᴀᴘᴛɪᴏɴ 🆓\n📦4ɢʙ ᴜᴘʟᴏᴀᴅ ʟɪᴍɪᴛ 👑\n📦3x ᴅᴏᴡɴʟᴏᴀᴅ sᴘᴇᴇᴅ👑\n📦ᴛʀɪᴍ ᴏʀ ᴍᴇʀɢᴇ ᴠɪᴅᴇᴏ 💎\n📦ᴀᴜᴅɪᴏ ᴄʜᴀɴɢᴇ ᴏʀ ʀᴇᴍᴏᴠᴇᴅ 💎\n\n **🆓 = all users can use\n👑 = only premium users can use\n💎 = only DIAMOND users can use**\n\n⚡**/upgrade 𝗣𝗟𝗔𝗡 𝗧𝗢 𝗨𝗦𝗘 𝗣𝗥𝗘𝗠𝗜𝗨𝗠 𝗙𝗘𝗔𝗧𝗨𝗥𝗘𝗦**",quote=True)
